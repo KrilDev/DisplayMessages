ce750066fc657e31dc267e17d8f23e00f5c89fec
Kril <sws_eac@post1.tele.dk>
2015-02-12 17:02:58 +0100

    Initial checkin of TestGitHubProject.

---
+++ TestGitHubProject/src/com/example/sws/DisplayAnotherMessage.java
@@ -0,0 +1,8 @@
+package com.example.sws;
+
+public class DisplayAnotherMessage {
+
+	public void displayAnotherMessage() {
+		System.out.println("Hello form Hello class");
+	}
+}
---
+++ TestGitHubProject/src/com/example/sws/DisplayMessages.java
@@ -0,0 +1,11 @@
+package com.example.sws;
+
+public class DisplayMessages {
+
+	public static void main(String[] args) {
+		DisplayAnotherMessage displayAnotherMessage = new DisplayAnotherMessage();
+		System.out.println("Hello from DisplayMessages class!");
+		displayAnotherMessage.displayAnotherMessage();
+	}
+
+}
